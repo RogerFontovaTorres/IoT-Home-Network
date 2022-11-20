614e569175be3454f9f14b26e44a00d19e6316ab
RogerFontovaTorres <79267515+RogerFontovaTorres@users.noreply.github.com>
2022-11-20 22:57:12 +0100

    Folders refactoring

--- iot-devices/src/iot-device.py
+++ iot-devices/src/iot-device.py
@@ -4,7 +4,7 @@ from random import seed
 import time
 import pandas as pd
 
-df = pd.read_csv("../data/temperature.csv")
+df = pd.read_csv("data/temperature.csv")
 
 for _, row in df.iterrows():
     publish.single("Temperature",
