5b87e6ee0b8cfc96354cfcc9b9858747d8e41408
Arturito <arturculleres@gmail.com>
2022-11-20 20:38:05 +0100

    feature #4: implemented communication between mqtt_publisher and mqtt_subscriber

--- mqtt/python-script/publish.py
+++ mqtt/python-script/publish.py
@@ -8,21 +8,6 @@ df = pd.read_csv("data.csv")
 
 for _, row in df.iterrows():
     publish.single("Temperature",
-    row,
+    str(row["Temperature"].item()),
     hostname="host.docker.internal")
     time.sleep(10)
-
-publish.single(f"ClassTest/status",
-               "up",
-               hostname="host.docker.internal")
-
-seed(1)
-for _ in range(10):
-    publish.single("ClassTest/temperature",
-                   random(),
-                   hostname="host.docker.internal")
-    time.sleep(1)
-
-publish.single(f"ClassTest/status",
-               "down",
-               hostname="host.docker.internal")
