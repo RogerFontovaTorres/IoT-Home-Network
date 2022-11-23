33b7f2146af4863421bd05ef629e35b911257b1f
Arturito <arturculleres@gmail.com>
2022-11-23 09:11:50 +0100

    fix #10: temperature values changed from string to double type

--- mqtt/python-script/publish.py
+++ mqtt/python-script/publish.py
@@ -3,11 +3,13 @@ from random import random
 from random import seed
 import time
 import pandas as pd
+from decimal import Decimal
+
 
 df = pd.read_csv("data.csv")
 
 for _, row in df.iterrows():
     publish.single("Temperature",
-    str(row["Temperature"].item()),
+    Decimal(row["Temperature"].item()),
     hostname="host.docker.internal")
     time.sleep(10)
